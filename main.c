3a360be13ffd36323db3c39788ea0e84b6bf4aa2
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2021-09-21 22:46:42 -0300

    commit final com o que foi contruido em tempo de prova

--- main.c
+++ main.c
@@ -1,19 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 // vertices e nós são a mesma coisas
+
 int *vetor_nos;
+
 int sizeArestas = 100;
 int qtdNos = 0;
 char *nomeDoArquivo;
-
-typedef struct Lista;
-
-typedef struct registro
-{
-    int visitado;
-    
-
-}registro;
 
 typedef struct No
 {
@@ -26,6 +19,13 @@ typedef struct Lista
     struct No *inicio;
 } Lista;
 
+typedef struct Registro
+{
+    int visitado;
+    struct Lista *lista;
+
+} Registro;
+Registro *registros;
 void verifica_size_vet(int a, int b)
 {
     if (a >= sizeArestas || b >= sizeArestas || qtdNos >= sizeArestas)
@@ -41,29 +41,79 @@ void verifica_size_vet(int a, int b)
         }
         if (qtdNos >= sizeArestas)
         {
-            sizeArestas += 101;
+            sizeArestas += (qtdNos - sizeArestas) + 100;
         }
+
         vetor_nos = (int *)realloc(vetor_nos, sizeArestas * sizeof(int));
+        registros = (Registro *)realloc(registros, sizeArestas * sizeof(Registro));
         for (int i = mem; i < sizeArestas; i++)
         {
+            registros[i].visitado = 0;
+            registros[i].lista = 0;
             vetor_nos[i] = 0;
         }
     }
 }
 
+No *novoNo(int valor)
+{
+    No *no = (No *)calloc(1, sizeof(No));
+    no->val = valor;
+    return no;
+}
 
-void push(No * no, int index){
-    
+Lista *NovaLista()
+{
+    return (Lista *)calloc(1, sizeof(Lista));
+}
+
+void inserir_ordenado(Lista *lista, int valor)
+{
+    if (lista->inicio)
+    {
+        No *no = lista->inicio;
+        if (no->val > valor)
+        {
+            lista->inicio = novoNo(valor);
+            No *temp = lista->inicio;
+            temp->prx = no;
+        }
+        else
+        {
+            while (no->prx && no->val > valor)
+            {
+                no = no->prx;
+            }
+            No *temp = no->prx;
+            no->prx = novoNo(valor);
+            no = no->prx;
+            no->prx = temp;
+        }
+    }
+    else
+    {
+        lista->inicio = novoNo(valor);
+    }
+}
+
+void push(Registro * registro, int index)
+{
+    if (!registro->lista)
+    {
+        registro->lista = NovaLista();
+    }
+    inserir_ordenado(registro->lista, index);
 }
 
 int main()
 {
-    FILE *arq;
-    arq = fopen(nomeDoArquivo, "r");
-    int a, b;
-    vetor_nos = (int *)calloc(sizeArestas, sizeof(int));
-    while (fscanf(arq, "%d:%d", &a, &b))
+    int a, b, v;
+    registros = (Registro *)calloc(sizeArestas, sizeof(Registro));
+    vetor_nos = (int *)realloc(vetor_nos, sizeArestas * sizeof(int));
+    scanf("%d", &v);
+    for (int i = 0; i < v; i++)
     {
+        scanf("%d:%d", &a, &b);
         printf("\n A: %d , B: %d", a, b);
 
         //realloca o vetor se maior que o ja alocado
@@ -79,7 +129,8 @@ int main()
             qtdNos++;
             vetor_nos[b] = 1;
         }
-        push(&vetor_nos[a], b);
-        push(&vetor_nos[b], a);
+        push(&registros[a], b);
+        push(&registros[b], a);
     }
+    return 0;
 }
